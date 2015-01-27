b64f16020cbb5db555d682266a83912b036c18d9
Jesse Squires <jesse.d.squires@gmail.com>
2015-01-26 20:38:51 -0800

    workspace setup and framework project setup.

---
+++ RSTDataSourceKit/RSTDataSourceKit/RSTDataSourceKit.h
@@ -0,0 +1,26 @@
+//
+//  Rosetta Stone
+//  http://product.rosettastone.com/news/
+//
+//
+//  Documentation
+//  http://cocoadocs.org/docsets/RSTDataSourceKit
+//
+//
+//  GitHub
+//  https://github.com/rosettastone/RSTDataSourceKit
+//
+//
+//  License
+//  Copyright (c) 2014 Rosetta Stone
+//  Released under a BSD license: http://opensource.org/licenses/BSD-3-Clause
+//
+
+#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
+
+//! Project version number for RSTDataSourceKit.
+FOUNDATION_EXPORT double RSTDataSourceKitVersionNumber;
+
+//! Project version string for RSTDataSourceKit.
+FOUNDATION_EXPORT const unsigned char RSTDataSourceKitVersionString[];
---
+++ RSTDataSourceKit/RSTDataSourceKitTests/RSTDataSourceKitTests.m
@@ -0,0 +1,30 @@
+//
+//  Rosetta Stone
+//  http://product.rosettastone.com/news/
+//
+//
+//  Documentation
+//  http://cocoadocs.org/docsets/RSTDataSourceKit
+//
+//
+//  GitHub
+//  https://github.com/rosettastone/RSTDataSourceKit
+//
+//
+//  License
+//  Copyright (c) 2014 Rosetta Stone
+//  Released under a BSD license: http://opensource.org/licenses/BSD-3-Clause
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+#import <OCMock/OCMock.h>
+
+@interface RSTDataSourceKitTests : XCTestCase
+
+@end
+
+@implementation RSTDataSourceKitTests
+
+
+@end
