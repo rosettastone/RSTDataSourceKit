29b28ea9d717bc3a10cc9c4e64380782d27bccd1
Jesse Squires <jsquires@rosettastone.com>
2015-01-27 17:44:11 -0800

    pod update. add helper scripts for pod install/update

---
+++ podinstall.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+cd Example/ && pod install && cd ..
+cd RSTDataSourceKit/ && pod install && cd ..
---
+++ podupdate.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+cd Example/ && pod update && cd ..
+cd RSTDataSourceKit/ && pod update && cd ..
